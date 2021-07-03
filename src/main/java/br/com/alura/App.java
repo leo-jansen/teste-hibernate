fb5cf965c534be053d47d70f54f355bfea595252
leo-jansen <janseleo@gmail.com>
2021-07-03 03:14:27 -0300

    teste de persistencia e correcao do persistence.xml

--- src/main/java/br/com/alura/App.java
+++ src/main/java/br/com/alura/App.java
@@ -1,13 +1,27 @@
 package br.com.alura;
 
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import br.com.alura.entities.Produto;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    Produto celular = new Produto("Xiome", "celular chines", new BigDecimal("1800"));
+
+    // classe que faz a conexao no BD
+    EntityManager entityManager = Persistence.createEntityManagerFactory("loja").createEntityManager();
+
+    // teste para persistir um produto
+    entityManager.getTransaction().begin();     // abrindo conexao
+    entityManager.persist(celular);             // persistindo o objeto
+    entityManager.getTransaction().commit();    // comitando a persistencia 
+    entityManager.close();                      // fechando a conexao
+  }
 }
