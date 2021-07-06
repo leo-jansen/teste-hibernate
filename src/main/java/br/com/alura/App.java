b186a39ad94063edcca2035e639fdad0d89a4e2c
leo-jansen <janseleo@gmail.com>
2021-07-06 01:06:40 -0300

    ciclo de vida do obj na JPA

--- src/main/java/br/com/alura/App.java
+++ src/main/java/br/com/alura/App.java
@@ -25,10 +25,24 @@ public class App {
     entityManager.persist(categoria);           // abrindo conexao
     entityManager.persist(celular);             // persistindo o objeto
     entityManager.flush();                      // atualiza no banco de dados todos os objetos MANAGED
-    entityManager.clear();                      // coloca todos os objetos em DEPASHED
-    celular = entityManager.merge(celular);     // Devolve um novo objeto MANAGED (necessica ter o construtor padrão) 
-    celular.setNome("Poco");                    
+    entityManager.clear();                      // coloca todos os objetos em DETACHED
+    celular = entityManager.merge(celular);     // devolve um novo objeto MANAGED (necessica ter o construtor padrão) 
+    celular.setNome("Poco");
+    entityManager.remove(celular);              // faz um delete no BD                   
     entityManager.getTransaction().commit();    // comita a atualização no BD 
     entityManager.close();                      // fechando a conexao
   }
 }
+
+//                               CICLO DE VIDA
+//                                                                        ________
+//        ___________               _________    |   commit()/flush()->  |   BD   |
+// new-> | TRANSIENT | persist()-> |         |   |<-find()/createQuery() |________|
+//        -----------              | MANAGED |----
+//                                 |_________|   |                   __________
+//                                               |close()/clear()-> | DETACHED |
+//                                               |    <-merge()     |__________|
+//                                               |
+//                                               |            _______________
+//                                               |remove()-> | REMOVED do BD |
+//                                                            ---------------
